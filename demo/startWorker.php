868d7b493d081f6453383349477329a6fa6bca10
Stuart Carnie <stuart.carnie@gmail.com>
2013-11-08 13:31:58 -0700

    Support REDIS_HOST and REDIS_PORT environment variables

--- demo/startWorker.php
+++ demo/startWorker.php
@@ -6,7 +6,10 @@ require_once '../lib/Qless/Client.php';
 require_once 'TestWorkerImpl.php';
 
 $queues = ['testQueue1','testQueue2'];
-$client = new Qless\Client('localhost',6380);
+$REDIS_HOST = getenv('REDIS_HOST') ?: 'localhost';
+$REDIS_PORT = getenv('REDIS_PORT') ?: 6380;
+
+$client = new Qless\Client($REDIS_HOST, $REDIS_PORT);
 $worker = new Qless\Worker("TestWorker_1", $queues, $client, 5);
 
 $worker->run();
