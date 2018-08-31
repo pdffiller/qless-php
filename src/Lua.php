8272135107a8080c9e770117ddb3f0eaaa65f49f
Serghei Iakovlev <serghei@phalconphp.com>
2018-08-31 12:57:24 +0300

    Cleaned up Lua class

--- src/Lua.php
+++ src/Lua.php
@@ -14,39 +14,77 @@ class Lua
     /**
      * @var \Redis
      */
-    protected $redisCli;
+    protected $redis;
+
     /**
      * @var string
      */
     protected $redisHost;
+
     /**
      * @var int
      */
-    protected $redisPort;
+    protected $redisPort = 6379;
+
     /**
-     * @var string
+     * Redis connection timeout (default is 0.0 meaning unlimited).
+     *
+     * @var float
+     */
+    protected $redisTimeout = 0.0;
+
+    /**
+     * @var ?string
      */
     protected $sha = null;
 
-    public function __construct($redis)
+    /**
+     * Lua constructor.
+     *
+     * @param array $config
+     */
+    public function __construct(array $config)
     {
-        $this->redisHost = $redis['host'];
-        $this->redisPort = $redis['port'];
+        $this->redisHost = $config['host'];
+        $this->redisPort = $config['port'] ?? 6379;
+        $this->redisTimeout = $config['timeout'] ?? 0.0;
 
-        $this->redisCli  = new \Redis();
-        $this->redisCli->connect($this->redisHost, $this->redisPort);
+        $this->redis = new \Redis();
+        $this->connect();
     }
 
-    public function run($command, $args)
+    /**
+     * Perform connection to the Redis.
+     *
+     * @return void
+     */
+    protected function connect()
+    {
+        $this->redis->connect($this->redisHost, $this->redisPort, $this->redisTimeout);
+    }
+
+    /**
+     * Run a Lua command serverside.
+     *
+     * @param string $command
+     * @param array $args
+     * @return mixed|null
+     *
+     * @throws QlessException
+     */
+    public function run($command, array $args)
     {
         if (empty($this->sha)) {
             $this->reload();
         }
+
         $luaArgs  = [$command, microtime(true)];
         $argArray = array_merge($luaArgs, $args);
-        $result = $this->redisCli->evalSha($this->sha, $argArray);
-        $error  = $this->redisCli->getLastError();
-        if ($error) {
+
+        $result = $this->redis->evalSha($this->sha, $argArray);
+        $error  = $this->redis->getLastError();
+
+        if ($error !== null) {
             $this->handleError($error);
             return null;
         }
@@ -54,37 +92,56 @@ class Lua
         return $result;
     }
 
+    /**
+     * Parse a Lua error and throw human readable exception.
+     *
+     * @param string $error
+     * @return void
+     *
+     * @throws QlessException
+     */
     protected function handleError($error)
     {
-        $this->redisCli->clearLastError();
+        $this->redis->clearLastError();
 
         throw QlessException::createExceptionFromError($error);
     }
 
+    /**
+     * Reloads the qless-core code.
+     *
+     * @return void
+     */
     protected function reload()
     {
-        $script    = file_get_contents(__DIR__ . '/qless-core/qless.lua', true);
+        $script = file_get_contents(__DIR__ . '/qless-core/qless.lua', true);
         $this->sha = sha1($script);
-        $res = $this->redisCli->script('exists', $this->sha);
+
+        $res = $this->redis->script('exists', $this->sha);
+
         if ($res[0] !== 1) {
-            $this->sha = $this->redisCli->script('load', $script);
+            $this->sha = $this->redis->script('load', $script);
         }
     }
 
     /**
-     * Removes all the entries from the default Redis database
+     * Removes all the entries from the default Redis database.
+     *
+     * @return void
      */
     public function flush()
     {
-        $this->redisCli->flushDB();
+        $this->redis->flushDB();
     }
 
     /**
-     * Reconnect to the Redis server
+     * Reconnect to the Redis server.
+     *
+     * @return void
      */
     public function reconnect()
     {
-        $this->redisCli->close();
-        $this->redisCli->connect($this->redisHost, $this->redisPort);
+        $this->redis->close();
+        $this->connect();
     }
 }
