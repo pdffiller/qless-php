335ee957b891205ecfc3b515133165df493e4beb
Serghei Iakovlev <serghei@phalconphp.com>
2018-09-03 13:27:00 +0300

    Improved tests & demo bootstrap

--- demo/handlerWorker.php
+++ demo/handlerWorker.php
@@ -4,11 +4,10 @@ use Qless\Client;
 use Qless\Worker;
 use Qless\Demo\JobHandler;
 
-require_once dirname(__FILE__) . '/../vendor/autoload.php';
-require_once dirname(__FILE__) . '/bootstrap.php';
+require_once __DIR__ . '/../tests/bootstrap.php';
 
 $queues = ['testQueue1', 'testQueue2'];
-$client = new Client(REDIS_HOST, REDIS_PORT);
+$client = new Client(REDIS_HOST, REDIS_PORT, REDIS_TIMEOUT);
 
 $worker = new Worker("WorkerTest_1", $queues, $client, 5);
 $worker->registerJobPerformHandler(JobHandler::class);
--- demo/startWorker.php
+++ demo/startWorker.php
@@ -3,11 +3,11 @@
 use Qless\Client;
 use Qless\Worker;
 
-require_once './bootstrap.php';
+require_once __DIR__ . '/../tests/bootstrap.php';
 
 $queues = ['testQueue1', 'testQueue2'];
 
-$client = new Client(REDIS_HOST, REDIS_PORT);
+$client = new Client(REDIS_HOST, REDIS_PORT, REDIS_TIMEOUT);
 $worker = new Worker("TestWorker_1", $queues, $client, 5);
 
 $worker->run();
