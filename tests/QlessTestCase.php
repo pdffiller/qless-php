96cf2e937b34f0a8fd9c3c263ac06df37afea94d
Serghei Iakovlev <serghei@phalconphp.com>
2018-09-03 14:19:24 +0300

    Cleaned up base qless test

--- tests/QlessTestCase.php
+++ tests/QlessTestCase.php
@@ -2,8 +2,9 @@
 
 namespace Qless\Tests;
 
-use Qless\Client;
 use PHPUnit\Framework\TestCase;
+use Qless\Client;
+use Qless\Tests\Support\RedisAwareTrait;
 
 /**
  * Qless\Tests\QlessTestCase
@@ -14,14 +15,10 @@ use PHPUnit\Framework\TestCase;
  */
 abstract class QlessTestCase extends TestCase
 {
+    use RedisAwareTrait;
+
     /**  @var Client */
     protected $client;
-
-    /** @var string */
-    protected $redisHost;
-
-    /** @var int */
-    protected $redisPort;
 
     /**
      * {@inheritdoc}
@@ -30,13 +27,11 @@ abstract class QlessTestCase extends TestCase
      */
     public function setUp()
     {
-        $this->redisHost = getenv('REDIS_HOST') ?: '127.0.0.1';
-        $this->redisPort = getenv('REDIS_PORT') ?: 6379;
+        $callback = function (string $host, int $port, float $timeout) {
+            return new Client($host, $port, $timeout);
+        };
 
-        $this->client = new Client(
-            $this->redisHost,
-            $this->redisPort
-        );
+        $this->client = call_user_func_array($callback, $this->getRedisConfig());
     }
 
     /**
