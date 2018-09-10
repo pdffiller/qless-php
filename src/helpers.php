a8ff85a29a5f8e99d6e72f974351586b01a25489
Serghei Iakovlev <serghei@phalconphp.com>
2018-09-10 09:22:54 +0300

    Simplify Qless\pcntl_sig_name

--- src/helpers.php
+++ src/helpers.php
@@ -2,29 +2,23 @@
 
 namespace Qless;
 
-if (function_exists('pcntl_sig_name')) {
-    /**
-     * Creates a human readable pcntl signal name by its code.
-     *
-     * @param  int $signal
-     * @return string
-     */
-    function pcntl_sig_name(int $signal) : string
-    {
-        static $constants;
+/**
+ * Creates a human readable pcntl signal name by its code.
+ *
+ * @param  int $signal
+ * @return string
+ */
+function pcntl_sig_name(int $signal) : string
+{
+    $all = get_defined_constants(true)['pcntl'];
 
-        if (!isset($contstants)) {
-            $all = get_defined_constants(true)['pcntl'];
+    $filtered = array_filter(array_keys($all), function ($k) {
+        return strpos($k, 'SIG') === 0 && strpos($k, 'SIG_') === false;
+    });
 
-            $filtered = array_filter(array_keys($all), function ($k) {
-                return strpos($k, 'SIG') === 0 && strpos($k, 'SIG_') === false;
-            });
+    $constants = array_flip(array_intersect_key($all, array_flip($filtered)));
 
-            $constants = array_flip(array_intersect_key($all, array_flip($filtered)));
+    unset($all, $filtered);
 
-            unset($all, $filtered);
-        }
-
-        return isset($constants[$signal]) ? $constants[$signal] : 'UNKNOWN';
-    }
+    return isset($constants[$signal]) ? $constants[$signal] : 'UNKNOWN';
 }
