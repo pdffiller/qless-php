6d89dcdedd0051f686deeea35bdc1b82b7dc14c0
Serghei Iakovlev <serghei@phalconphp.com>
2018-09-13 17:47:55 +0300

    Improved procline helper

--- src/helpers.php
+++ src/helpers.php
@@ -26,11 +26,22 @@ function pcntl_sig_name(int $signal): string
  * Sets the process status.
  *
  * NOTE: Not supported on all systems.
+ * NOTE:
  *
  * @param  string $value
  * @return void
  */
 function procline(string $value): void
 {
-    cli_set_process_title('Qless PHP: ' . $value);
+    if (false === @cli_set_process_title($value)) {
+        if ('Darwin' === PHP_OS) {
+            \trigger_error(
+                'Running "cli_get_process_title" as an unprivileged user is not supported on macOS.',
+                E_USER_WARNING
+            );
+        } else {
+            $error = error_get_last();
+            trigger_error($error['message'], E_USER_WARNING);
+        }
+    }
 }
