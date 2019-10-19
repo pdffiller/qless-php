fc01a8b21b5e435d486886ae539c4d24c37fc7f2
Stephen Reay <stephen@koalephant.com>
2019-10-20 01:43:01 +0700

    Initial working example for a PubSub Manager, aka Qless-native Events
    This roughly follows the Python (and to a lesser extent Ruby) API, where `Client` exposes an `events` property which has `on(name, callback)` and `listen()` methods.
    
    I noticed the existing Watchdog PubSub consumer does a clone/disconnect/reconnect, but that method does not seem to reliably give a new connection (to allow the callback to use `Client` for other calls).
    
    The current implementation also doesn't offer the priority functionality of the existing callback based "Events" system - I thought it more important to keep parity with the upstream feature.
    
    I had to make a guess about naming too, it's not clear to me whether this class should be in the root `Qless` namespace, or even if you have some better name than `PubSub Manager` - EventsManager is taken and I assume you wouldn't just drop the old implementation.
    
    I've done some very minimal tests of this and it works as expected across processes. It'd be great to get some feedback in terms of naming/structure/etc and I can refine it from there.

---
+++ src/PubSub/Manager.php
@@ -0,0 +1,182 @@
+<?php
+
+namespace Qless\PubSub;
+
+use Predis\PubSub\Consumer;
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
+use Qless\Client;
+use Qless\Exceptions\InvalidArgumentException;
+use Predis\Client as Redis;
+use Ramsey\Uuid\Exception\InvalidUuidStringException;
+use Ramsey\Uuid\Uuid;
+
+/**
+ * Qless\PubSub\Manager
+ *
+ * Listen for qless-core notifications via PubSub
+ *
+ * @package Qless\PubSub
+ */
+final class Manager implements LoggerAwareInterface
+{
+
+    public const EVENT_CANCELED = 'canceled';
+    public const EVENT_COMPLETED = 'completed';
+    public const EVENT_FAILED = 'failed';
+    public const EVENT_POPPED = 'popped';
+    public const EVENT_STALLED = 'stalled';
+    public const EVENT_PUT = 'put';
+    public const EVENT_TRACK = 'track';
+    public const EVENT_UNTRACK = 'untrack';
+
+    /**
+     * @var Redis
+     */
+    public $redis;
+
+    /**
+     * @var Client
+     */
+    protected $client;
+
+    /** @var LoggerInterface */
+    private $logger;
+
+    /**
+     * @var \Closure[][]
+     */
+    protected $callbacks = [
+        self::EVENT_CANCELED => [],
+        self::EVENT_COMPLETED => [],
+        self::EVENT_FAILED => [],
+        self::EVENT_POPPED => [],
+        self::EVENT_STALLED => [],
+        self::EVENT_PUT => [],
+        self::EVENT_TRACK => [],
+        self::EVENT_UNTRACK => []
+    ];
+
+    public function __construct(Client $client, Redis $redis)
+    {
+        $this->client = $client;
+        $this->redis = $redis;
+        $this->logger = new NullLogger();
+    }
+
+    /**
+     * Convert an event name to a Qless PUBSUB channel name
+     *
+     * @param string $event
+     *
+     * @return string
+     */
+    protected static function channelName(string $event): string
+    {
+        return "ql:{$event}";
+    }
+
+    /**
+     * Convert a Qless PUBSUB channel name to an event name
+     *
+     * @param string $channel
+     *
+     * @return string
+     */
+    protected static function eventName(string $channel): string
+    {
+        return \preg_replace('/^ql:/', '', $channel);
+    }
+
+    /**
+     * Listen for Events of the given type.
+     *
+     * @param string $event The event type. Should be one of the PubSub\Manager::EVENT_* constants.
+     * @param callable $callback The callback to invoke when the event occurs.
+     *
+     * @return $this
+     */
+    public function on(string $event, callable $callback): self
+    {
+        if (! \array_key_exists($event, $this->callbacks)) {
+            throw new InvalidArgumentException('event must be a known event type');
+        }
+
+        $this->callbacks[$event][] = \Closure::fromCallable($callback);
+
+        return $this;
+    }
+
+    /**
+     * Listen for Events, and invoke registered callbacks.
+     */
+    public function listen(): void
+    {
+        $channels = \array_map([static::class, 'channelName'], \array_keys($this->callbacks));
+
+        $this->redis->connect();
+
+        /** @var Consumer $pubSub */
+        $pubSub = $this->redis->pubSubLoop();
+        $pubSub->subscribe(...$channels);
+        /** @var \stdClass $message */
+        foreach ($pubSub as $message) {
+            $this->handleMessage($message);
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @param  LoggerInterface $logger
+     * @return void
+     */
+    public function setLogger(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+
+    /**
+     * Handle a received message
+     *
+     * @param \stdClass $message
+     */
+    protected function handleMessage(\stdClass $message): void
+    {
+        if ($message->kind !== 'message' || empty($message->payload)) {
+            return;
+        }
+
+        $jid = $message->payload;
+        if (empty($jid)) {
+            return;
+        }
+
+        try {
+            Uuid::fromString($jid);
+        } catch (InvalidUuidStringException $exception) {
+            $this->logger->warning('Invalid JID received: ' . $jid, \compact('message'));
+            return;
+        }
+
+        $event = static::eventName($message->channel);
+
+        if (! array_key_exists($event, $this->callbacks)) {
+            $this->logger->warning('Unknown event type received: ' . $event, \compact('message'));
+            return;
+        }
+
+        foreach ($this->callbacks[$event] as $closure) {
+            try {
+                $closure($jid);
+            } catch (\Throwable $exception) {
+                $this->logger->error(
+                    'Error while running callback: ' . $exception->getMessage(),
+                    \compact('jid', 'event', 'exception')
+                );
+            }
+        }
+    }
+}
